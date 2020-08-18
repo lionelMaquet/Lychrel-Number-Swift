b2d2ba37f91178ef621fed6d8fd3a7e7f9bf80f4
Lionel <lionel.maquet@gmail.com>
2020-08-18 18:15:16 +0200

    adds revertsAndAdd function

--- Lychrel Number/ViewController.swift
+++ Lychrel Number/ViewController.swift
@@ -13,8 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(revert(number: 514678937204632))
     }
-
+    
+    func revertAndAdd(number: Int) -> Int {
+        return number + revert(number: number)
+    }
+    
+    func revert(number: Int) -> Int {
+        var numberArray = [Character]()
+        for i in String(number) {
+            numberArray.insert(i, at:0)
+        }
+        var finalNumberString = ""
+        
+        for i in numberArray {
+            finalNumberString = "\(finalNumberString)\(i)"
+        }
+        
+        let revertedNumber = Int(finalNumberString)
+        
+        return revertedNumber!
+        
+    }
 
 }
 
