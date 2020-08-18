e79584b2e8b666d155f54bc20880dce09db9bc63
Lionel <lionel.maquet@gmail.com>
2020-08-18 19:00:34 +0200

    adds LychrelHistoric object and ends main functionalities

--- Lychrel Number/ViewController.swift
+++ Lychrel Number/ViewController.swift
@@ -12,13 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print(revertAndAdd(number: 9435239 ))
-        print(isPalindrome(number: 1001))
+        
+        let numberToTry = 1998
+        let lych = lychrelProcess(number: numberToTry, lychrelHistoric: nil)
+        print("Might your number \(numberToTry) be a lychrel number ? Answer is \(lych.mightBeLychrel) \n")
+        print("Number of operations tried : \(lych.numberOfOperations) \n")
+        print("Historic of operations : \(lych.historicOfOperations)")
+        
     }
     
     func revertAndAdd(number: Int) -> Int {
-        return number + revert(number: number)
+        
+        if (revert(number: number) != -1){
+            return number + revert(number: number)
+        } else {
+            return -1 // stop signal
+        }
+        
     }
     
     func revert(number: Int) -> Int {
@@ -27,6 +37,10 @@ class ViewController: UIViewController {
             numberArray.insert(i, at:0)
         }
         var finalNumberString = ""
+        
+        if numberArray.count > 16 {
+            return -1 // stop signal. too big of a number, we can't revert and add again.
+        }
         
         for i in numberArray {
             finalNumberString = "\(finalNumberString)\(i)"
@@ -40,6 +54,36 @@ class ViewController: UIViewController {
     
     func isPalindrome(number: Int) -> Bool {
         return number == revert(number: number)
+    }
+    
+    func lychrelProcess(number: Int, lychrelHistoric : LychrelHistoric?) -> LychrelHistoric {
+        
+        let lychrelHist : LychrelHistoric
+        
+        if let sureLychrelHistoric = lychrelHistoric {
+            lychrelHist = sureLychrelHistoric
+        } else {
+            lychrelHist = LychrelHistoric()
+        }
+        lychrelHist.historicOfOperations.append(number)
+        
+        let newNumber = revertAndAdd(number: number)
+        
+        if (newNumber != -1){
+            
+            if (!isPalindrome(number: newNumber)){
+                //lychrelHist.historicOfOperations.append(newNumber)
+                lychrelProcess(number: newNumber, lychrelHistoric: lychrelHist)
+            } else {
+                lychrelHist.historicOfOperations.append(newNumber)
+                lychrelHist.mightBeLychrel = false
+                return lychrelHist
+            }
+        } else {
+            return lychrelHist
+        }
+    
+        return lychrelHist
     }
 
 }
