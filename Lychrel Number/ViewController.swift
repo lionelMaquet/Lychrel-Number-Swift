da20e969bd507a14a9f9a592693ef1122006bd91
Lionel <lionel.maquet@gmail.com>
2020-08-18 17:57:36 +0200

    Initial Commit

---
+++ Lychrel Number/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lychrel Number
+//
+//  Created by Lionel Maquet on 18/08/2020.
+//  Copyright © 2020 Lionel Maquet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
